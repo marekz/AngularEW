95d5251004fa8728319f80947923edd1db3b19fe
Marek Zdybel <marek@motta.com.pl>
2018-08-01 23:14:07 +0200

    Work with DOM

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Witaj w kursie';
+
+  selected = null;
+  
+  playlist = {
+    name: 'The best of eduweb!',
+    tracks: 23,
+    color: '#FF0000',
+    favorite: true
+  }
+
+  save(event){
+    console.log('Zapisano', event);
+  }
+
+  constructor(){
+
+  }
+}
