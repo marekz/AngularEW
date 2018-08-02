0d562e69b1188a2ceb7ff065178287e075212383
Marek Zdybel <marek@motta.com.pl>
2018-08-02 21:14:19 +0200

    Style encapsulation

---
+++ src/app/playlists/content-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-content-card',
+  template: `
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">Playlista</h4>
+      <p class="card-text">Wybierz Playlistę</p>
+    </div>
+  </div>
+  `,
+  styles: [`
+    h4 {
+      color: red;
+    }
+  `],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ContentCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
